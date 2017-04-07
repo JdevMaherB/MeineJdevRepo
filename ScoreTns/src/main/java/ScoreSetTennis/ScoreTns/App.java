3675d10d250f3719b7759a64a976990b8e0fbabc
JdevMaherB <jdev.maherb@gmail.com>
2017-04-07 11:52:47 +0200

    Creation of an empty maven project

---
+++ ScoreTns/src/main/java/ScoreSetTennis/ScoreTns/App.java
@@ -0,0 +1,13 @@
+package ScoreSetTennis.ScoreTns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
